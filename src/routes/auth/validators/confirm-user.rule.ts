471f6cd8f848c0caf77e02427a4d62d22a1ec89b
Lakshmipriya Mukundan <lakshmipriya.m@cubettech.com>
2018-10-31 12:09:16 +0530

    auth folder removed

--- src/routes/auth/validators/confirm-user.rule.ts
+++
@@ -1,7 +0,0 @@
-import { query } from 'express-validator/check';
-
-export const confirmUserRule = [
-  query('token')
-    .exists()
-    .withMessage('Invalid token'),
-];
--- src/startup/reset-database.ts
+++ src/startup/reset-database.ts
@@ -4,10 +4,6 @@ import { Promise as BluePromise } from 'bluebird';
 import * as mongoose from 'mongoose';
 import { getMongooseConnectionPromise } from './db-init';
 
-import { initUsers } from './users';
-import { createCat } from './add-cat-subcat';
-import { createProjCat } from './add-proj-cat-subcat';
-import { createAvail } from './add-availability-calender';
 
 import * as lme from 'lme';
 
@@ -30,40 +26,6 @@ const resetDatabase = async (MONGO_URI?: string) => {
       mongoose.connection.db
         .dropCollection('businessfunctions')
         .catch(errHandler),
-      mongoose.connection.db.dropCollection('industries').catch(errHandler),
-      mongoose.connection.db.dropCollection('users').catch(errHandler),
-      mongoose.connection.db.dropCollection('tempusers').catch(errHandler),
-      mongoose.connection.db
-        .dropCollection('availabilitycalenders')
-        .catch(errHandler),
-      mongoose.connection.db.dropCollection('timeslots').catch(errHandler),
-      mongoose.connection.db
-        .dropCollection('skillsubcategories')
-        .catch(errHandler),
-      mongoose.connection.db
-        .dropCollection('skillcategories')
-        .catch(errHandler),
-      mongoose.connection.db.dropCollection('educations').catch(errHandler),
-      mongoose.connection.db
-        .dropCollection('employeeprojects')
-        .catch(errHandler),
-      mongoose.connection.db
-        .dropCollection('personaldetails')
-        .catch(errHandler),
-      mongoose.connection.db.dropCollection('experiences').catch(errHandler),
-      mongoose.connection.db
-        .dropCollection('customercredentials')
-        .catch(errHandler),
-      mongoose.connection.db
-        .dropCollection('projectcategories')
-        .catch(errHandler),
-      mongoose.connection.db
-        .dropCollection('projectsubcategories')
-        .catch(errHandler),
-      mongoose.connection.db.dropCollection('projects').catch(errHandler),
-      mongoose.connection.db
-        .dropCollection('interviewavailabilitycalenders')
-        .catch(errHandler),
     ]);
   } catch (err) {
     if (err.code === 26 || err.message === 'ns not found') {
@@ -75,12 +37,6 @@ const resetDatabase = async (MONGO_URI?: string) => {
     }
   }
 
-  try {
-    await BluePromise.all([initUsers(), createCat(), createProjCat()]);
-    await createAvail(); // heavy task. thats why running alone
-  } catch (err) {
-    console.log(err);
-  }
 
   try {
     await mongoose.disconnect();
