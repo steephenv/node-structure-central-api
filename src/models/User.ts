d77eac2bae211b38912b31c3cda54320d0de6839
Lakshmipriya Mukundan <lakshmipriya.m@cubettech.com>
2018-10-31 14:51:45 +0530

    User model added

---
+++ src/models/User.ts
@@ -0,0 +1,71 @@
+/* tslint:disable:variable-name */
+
+import { model as mongooseModel, Schema } from 'mongoose';
+
+export const description = 'Stores details of user info';
+
+export const definitions = {
+ firstNameEng: String,
+ middleNameEng: String,
+ lastNameEng: String,
+ commonNameEng: String,
+ genderEng: String,
+ nationalIdEng: String,
+ mobileEng: String,
+ email: {
+   type: String,
+   unique: true,
+   required: true
+ },
+ educationEng: String,
+ profilePic: String,
+ houseAptEng: String,
+ streetEng: String,
+ atollEng: String,
+ islandEng: String,
+ cityEng: String,
+ postCodeEng: String,
+ countryEng: String,
+ employeeIdEng: String,
+ joinDateEng: Date,
+ presentPostTitleEng: String,
+ sectionEng: String,
+ divisionEng: String,
+ unitEng: String,
+ statusEng: String,
+
+ firstNameXXX: String,
+ middleNameXXX: String,
+ lastNameXXX: String,
+ commonNameXXX: String,
+ genderXXX: String,
+ nationalIdXXX: String,
+ mobileXXX: String,
+ emailXXX: {
+   type: String,
+   unique: true,
+   required: true
+ },
+ educationXXX: String,
+ houseAptXXX: String,
+ streetXXX: String,
+ atollXXX: String,
+ islandXXX: String,
+ cityXXX: String,
+ postCodeXXX: String,
+ countryXXX: String,
+ employeeIdXXX: String,
+ joinDateXXX: Date,
+ presentPostTitleXXX: String,
+ sectionXXX: String,
+ divisionXXX: String,
+ unitXXX: String,
+ statusXXX: String
+
+
+};
+
+const userSchema: Schema = new Schema(definitions);
+
+
+export const User = mongooseModel('User', userSchema);
