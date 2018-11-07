a029c1adfa2b3bdcaaf0033c21fa1d036a8aa212
Lakshmipriya Mukundan <lakshmipriya.m@cubettech.com>
2018-11-07 17:07:42 +0530

    permission model basic added

---
+++ src/models/Permission.ts
@@ -0,0 +1,12 @@
+/* tslint:disable:variable-name */
+
+import { model as mongooseModel, Schema } from 'mongoose';
+
+export const description = `For adding permissions`;
+export const definitions = {
+  permission: { type: String },
+};
+
+const permissionSchema = new Schema(definitions);
+
+export const Permission = mongooseModel('permission', permissionSchema);
