af4fbae3193d05641430ebcb553fc5026c98cedf
Lakshmipriya Mukundan <lakshmipriya.m@cubettech.com>
2018-11-08 12:16:40 +0530

    user start script change

--- src/startup/users.ts
+++ src/startup/users.ts
@@ -4,7 +4,7 @@ import { Promise as BluePromise } from 'bluebird';
 const user1 = new User({
   email: 'sherlock@yopmail.com',
   password: '123456',
-  role: 'BPM',
+  role: '5be3c9d9c7311236010d35a8',
 });
 
 const user2 = new User({
@@ -12,7 +12,7 @@ const user2 = new User({
   lastName: 'Shah',
   email: 'bernu@yopmail.com',
   password: '123456',
-  role: 'Client',
+  role: '5be3c9d9c7311236010d35a8',
 });
 
 export const initUsers = async () => {
