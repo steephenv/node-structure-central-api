cdddeed2751698d8e8eb50ba512d850f1d581915
Lakshmipriya Mukundan <lakshmipriya.m@cubettech.com>
2018-11-12 15:20:43 +0530

    isDelete field added in permission

--- src/models/Permission.ts
+++ src/models/Permission.ts
@@ -5,6 +5,7 @@ import { model as mongooseModel, Schema } from 'mongoose';
 export const description = `For adding permissions`;
 export const definitions = {
   permissions: [{ type: String }],
+  isDelete: { type: Boolean, default: false },
 };
 
 const permissionSchema = new Schema(definitions);
