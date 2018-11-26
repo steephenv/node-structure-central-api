b4b3f80d15e4188d56345fd10255673e0ddc9217
Lakshmipriya Mukundan <lakshmipriya.m@cubettech.com>
2018-11-26 13:12:27 +0530

    db index change

--- src/models/index.ts
+++ src/models/index.ts
@@ -19,6 +19,8 @@ import { CaseParticipantRole as caseparticipantrole } from './CaseParticipantsRo
 import { DepartmentSection as departmentsection } from './DepartmentSection';
 import { Atoll as atoll } from './Atoll';
 import { Island as island } from './Island';
+import { Contact as contact } from './Contacts';
+import { Task as task } from './Task';
 
 export const Models: { [key: string]: typeof AppData } = {
   user,
@@ -40,4 +42,6 @@ export const Models: { [key: string]: typeof AppData } = {
   departmentsection,
   atoll,
   island,
+  contact,
+  task,
 };
