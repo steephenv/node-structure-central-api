c248e6f5c73663444752e7c3861d665974046c46
Lakshmipriya Mukundan <lakshmipriya.m@cubettech.com>
2018-12-06 18:05:11 +0530

    document shema changes

--- src/models/Document.ts
+++ src/models/Document.ts
@@ -7,6 +7,19 @@ export const definitions = {
   case: { type: Schema.Types.ObjectId, ref: 'CaseDetails', required: true },
   docTitle: { type: String, required: true },
   docUrl: { type: String, required: true },
+
+  docCategory: { type: String },
+  docStage: { type: String },
+  docSummary: { type: String },
+  docType: { type: String },
+  documentDate: { type: Date },
+  pageCount: { type: Number },
+  secretary: { type: String },
+  sentBy: { type: String },
+  sentByOrg: { type: Boolean },
+  ward: { type: String },
+  receiveSection: { type: Schema.Types.ObjectId, ref: 'DepartmentSection' },
+
   physicalLoc: { type: Schema.Types.ObjectId, ref: 'Shelf' },
   accessRights: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   lastAccessBy: { type: Schema.Types.ObjectId, ref: 'User' },
@@ -18,6 +31,19 @@ export const definitions = {
       accessedBy: { type: Schema.Types.ObjectId, ref: 'User' },
     },
   ],
+  atoll: {
+    type: Schema.Types.ObjectId,
+    ref: 'Atoll',
+    comment:
+      'has the from `objectId[]`. objectId taken from `Atoll` collection',
+  },
+  island: {
+    type: Schema.Types.ObjectId,
+    ref: 'Island',
+    comment:
+      'has the from `objectId[]`. objectId taken from `Island` collection',
+  },
+  code: { type: String, required: true },
   createdAt: { type: Date },
   updatedAt: { type: Date },
   isDelete: {
