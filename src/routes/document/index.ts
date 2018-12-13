2828df097a63c0e678d2bc25cdd3531cfad9a8d3
Lakshmipriya Mukundan <lakshmipriya.m@cubettech.com>
2018-12-13 14:49:05 +0530

    copy doc api added

--- src/routes/document/index.ts
+++ src/routes/document/index.ts
@@ -1,7 +1,9 @@
 import * as express from 'express';
 
 import { listDocsFolders } from '../document/list-documents';
+import { updateDocs } from '../document/update-doc';
 
 export const docs = express.Router();
 
 docs.get('/list-folders', listDocsFolders);
+docs.post('/update-docs', updateDocs);
