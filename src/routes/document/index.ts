b3e2b13a7fabad8b2f1c345eac611807899fb073
Lakshmipriya Mukundan <lakshmipriya.m@cubettech.com>
2018-12-13 16:23:31 +0530

    move document functionality added

--- src/routes/document/index.ts
+++ src/routes/document/index.ts
@@ -4,8 +4,10 @@ import { docCopyRules } from './validators/copy-doc-rule';
 
 import { listDocsFolders } from '../document/list-documents';
 import { copyDoc } from '../document/copy-doc';
+import { moveDocument } from '../document/move-doc';
 
 export const docs = express.Router();
 
 docs.get('/list-folders', listDocsFolders);
 docs.post('/copy-doc', docCopyRules, copyDoc);
+docs.post('/move-doc', docCopyRules, moveDocument);
