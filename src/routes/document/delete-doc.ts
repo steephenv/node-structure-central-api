20e3a21d03ea4a4ceb566461663760f2abf9a81f
Lakshmipriya Mukundan <lakshmipriya.m@cubettech.com>
2018-12-13 17:45:35 +0530

    delete doc functionality added

---
+++ src/routes/document/delete-doc.ts
@@ -0,0 +1,27 @@
+/* tslint:disable:no-console */
+import { RequestHandler } from 'express';
+import {
+  RequestError,
+  RequestErrorType,
+} from '../../error-handler/RequestError';
+
+import { Document as Doc } from '../../models/Document';
+
+import { unlinkFunction } from './move-doc';
+
+export const delDocument: RequestHandler = async (req, res, next) => {
+  try {
+    const document: any = await Doc.findOne({ _id: req.query.docId })
+      .lean()
+      .exec();
+
+    await unlinkFunction(document.docUrl);
+
+    await Doc.remove({ _id: req.query.docId }).exec();
+
+    return res.status(201).send({ success: true });
+  } catch (err) {
+    console.log(err);
+    return next(new RequestError(RequestErrorType.BAD_REQUEST, err));
+  }
+};
