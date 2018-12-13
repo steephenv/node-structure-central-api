ea0c6499860849415925aa41adc2ee80a7b08898
Lakshmipriya Mukundan <lakshmipriya.m@cubettech.com>
2018-12-13 14:58:34 +0530

    copy doc route added

--- src/routes/document/index.ts
+++ src/routes/document/index.ts
@@ -1,9 +1,11 @@
 import * as express from 'express';
 
+import { docCopyRules } from './validators/copy-doc-rule';
+
 import { listDocsFolders } from '../document/list-documents';
 import { copyDoc } from '../document/copy-doc';
 
 export const docs = express.Router();
 
 docs.get('/list-folders', listDocsFolders);
-docs.post('/copy-doc', copyDoc);
+docs.post('/copy-doc', docCopyRules, copyDoc);
