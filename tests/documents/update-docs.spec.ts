eb7d5ddfc80ca4b2ac4f12c0034ee9f2923534a0
Lakshmipriya Mukundan <lakshmipriya.m@cubettech.com>
2018-12-13 14:49:18 +0530

    copy doc test file added

---
+++ tests/documents/update-docs.spec.ts
@@ -0,0 +1,26 @@
+import * as got from 'got';
+
+describe('Test for Move docs  ===> ', () => {
+  it(
+    'Move docs',
+    done => {
+      got('http://localhost:7000/v1/documents/copy-doc', {
+        method: 'POST',
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest',
+        },
+        json: true,
+        body: {
+          destinationUrl: '/test-images/new',
+          operation: 'copy',
+          docId: '5c10d160a258192a68f958ab',
+        },
+      })
+        .then(() => done())
+        .catch(err => {
+          throw err;
+        });
+    },
+    15000,
+  );
+});
