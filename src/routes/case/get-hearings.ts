1b6736fba00480bf3b976c75b41353eb70d3b328
Lakshmipriya Mukundan <lakshmipriya.m@cubettech.com>
2018-12-19 15:09:26 +0530

    get hearing api added

--- src/routes/case/get-hearings.ts
+++ src/routes/case/get-hearings.ts
@@ -1,35 +1,58 @@
 /* tslint:disable:no-console */
 import { RequestHandler } from 'express';
+import * as mongoose from 'mongoose';
 
 import {
   RequestError,
   RequestErrorType,
 } from '../../error-handler/RequestError';
 
-import { CaseDetails } from '../../models/Case';
+import { CaseHearing } from '../../models/CaseHearing';
 // import * as lme from 'lme';
 
 export const fetchHearings: RequestHandler = async (req, res, next) => {
   try {
-    const startTime = new Date(new Date().setUTCHours(0, 0, 0, 0));
-    const endTime = new Date(new Date().setUTCHours(23, 59, 59, 999));
+    let hearingData: any = [];
+    if (req.query && req.query.caseId) {
+      hearingData = await CaseHearing.find({
+        caseId: mongoose.Types.ObjectId(req.query.caseId),
+        isDelete: false,
+      })
+        .populate([
+          { path: 'caseId', model: 'CaseDetails' },
+          {
+            path: 'attorney',
+            model: 'User',
+            select: { firstName: 1, lastName: 1 },
+          },
+          {
+            path: 'caseHearingType',
+            model: 'CaseHearingType',
+          },
+        ])
+        .exec();
+    } else {
+      const startTime = new Date(new Date().setUTCHours(0, 0, 0, 0));
+      const endTime = new Date(new Date().setUTCHours(23, 59, 59, 999));
 
-    const hearingData = await CaseDetails.find({
-      isDelete: false,
-      'caseHearings.hearingDate': { $gte: startTime, lte: endTime },
-    })
-      .populate([
-        {
-          path: 'caseHearings.caseHearingType',
-          model: 'CaseHearingType',
-        },
-        {
-          path: 'caseHearings.attorney',
-          model: 'User',
-          select: { firstName: 1, lastName: 1 },
-        },
-      ])
-      .exec();
+      hearingData = await CaseHearing.find({
+        isDelete: false,
+        hearingDate: { $gte: startTime, lte: endTime },
+      })
+        .populate([
+          { path: 'caseId', model: 'CaseDetails' },
+          {
+            path: 'attorney',
+            model: 'User',
+            select: { firstName: 1, lastName: 1 },
+          },
+          {
+            path: 'caseHearingType',
+            model: 'CaseHearingType',
+          },
+        ])
+        .exec();
+    }
     return res.send({ success: true, data: hearingData });
   } catch (err) {
     console.log(err);
