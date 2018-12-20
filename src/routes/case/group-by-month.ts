b2f67806bd60f9a072e3137fa3a79da89ababa03
Lakshmipriya Mukundan <lakshmipriya.m@cubettech.com>
2018-12-20 19:23:37 +0530

    group by month data added

--- src/routes/case/group-by-month.ts
+++ src/routes/case/group-by-month.ts
@@ -11,19 +11,19 @@ import { CaseDetails } from '../../models/Case';
 
 export const groupByMonth: RequestHandler = async (req, res, next) => {
   try {
-    const monthArray: string[] = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
+    const monthFullData = [
+      { monthName: 'January', totalCases: 0, month: 1 },
+      { monthName: 'February', totalCases: 0, month: 2 },
+      { monthName: 'March', totalCases: 0, month: 3 },
+      { monthName: 'April', totalCases: 0, month: 4 },
+      { monthName: 'May', totalCases: 0, month: 5 },
+      { monthName: 'June', totalCases: 0, month: 6 },
+      { monthName: 'July', totalCases: 0, month: 7 },
+      { monthName: 'August', totalCases: 0, month: 8 },
+      { monthName: 'September', totalCases: 0, month: 9 },
+      { monthName: 'October', totalCases: 0, month: 10 },
+      { monthName: 'November', totalCases: 0, month: 11 },
+      { monthName: 'December', totalCases: 0, month: 12 },
     ];
 
     const monthWiseData = await CaseDetails.aggregate([
@@ -40,11 +40,21 @@ export const groupByMonth: RequestHandler = async (req, res, next) => {
       },
     ]).exec();
 
-    const monthResult = monthWiseData.map((obj: any) => {
-      obj._id.monthName = monthArray[obj._id.month - 1];
-      return obj;
-    });
-    return res.send({ success: true, data: monthResult });
+    if (!monthWiseData.length) {
+      return monthFullData;
+    } else {
+      // await new BluePromise((resolve, reject) => {});
+      for (const mf of monthFullData) {
+        for (const mw of monthWiseData) {
+          if (mf.month === mw._id.month) {
+            mf.totalCases = mw.totalCases;
+            break;
+          }
+        }
+      }
+    }
+
+    return res.send({ success: true, data: monthFullData });
   } catch (err) {
     console.log(err);
     return next(new RequestError(RequestErrorType.INTERNAL_SERVER_ERROR, err));
