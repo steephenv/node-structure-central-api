11a6805a506a8e309563d52ded4fbdd33353282f
Lakshmipriya Mukundan <lakshmipriya.m@cubettech.com>
2018-12-31 18:12:08 +0530

    hearing get api added

--- src/routes/case/get-hearings.ts
+++ src/routes/case/get-hearings.ts
@@ -6,40 +6,114 @@ import {
   RequestError,
   RequestErrorType,
 } from '../../error-handler/RequestError';
+import { Promise as BluePromise } from 'bluebird';
 
 import { CaseHearing } from '../../models/CaseHearing';
-// import * as lme from 'lme';
+import { CaseDetails } from '../../models/Case';
+import { User } from '../../models/User';
+
+import * as lme from 'lme';
 
 export const fetchHearings: RequestHandler = async (req, res, next) => {
   try {
     let limit = 50;
     let skip = 0;
-    let condition: any = {};
+    const condition: any = { isDelete: false };
+    let caseIds: any = [];
     if (req.query && req.query.limit) {
-      limit = req.query.limit;
+      limit = parseInt(req.query.limit, 10);
     }
     if (req.query && req.query.skip) {
-      skip = req.query.skip;
+      skip = parseInt(req.query.skip, 10);
     }
-    if (req.query && req.query.caseId) {
-      condition = {
-        caseId: mongoose.Types.ObjectId(req.query.caseId),
-        isDelete: false,
-      };
-    } else if (req.query && req.query.date) {
-      const startTime = new Date(
-        new Date(req.query.date).setUTCHours(0, 0, 0, 0),
-      );
-      const endTime = new Date(
-        new Date(req.query.date).setUTCHours(23, 59, 59, 999),
-      );
-      condition = {
-        isDelete: false,
-        hearingDate: { $gte: startTime, $lte: endTime },
-      };
-    } else {
-      condition = { isDelete: false };
+    if (req.query) {
+      if (req.query.caseId) {
+        condition.caseId = mongoose.Types.ObjectId(req.query.caseId);
+      }
+      if (req.query.date) {
+        const startTime = new Date(
+          new Date(req.query.date).setUTCHours(0, 0, 0, 0),
+        );
+        const endTime = new Date(
+          new Date(req.query.date).setUTCHours(23, 59, 59, 999),
+        );
+        condition.hearingDate = { $gte: startTime, $lte: endTime };
+      }
+      if (req.query.startDate && req.query.endDate) {
+        const startTime = new Date(
+          new Date(req.query.startDate).setUTCHours(0, 0, 0, 0),
+        );
+        const endTime = new Date(
+          new Date(req.query.endDate).setUTCHours(23, 59, 59, 999),
+        );
+        condition.hearingDate = { $gte: startTime, $lte: endTime };
+      }
+      if (req.query.searchText) {
+        const caseIDs = CaseDetails.find({
+          $or: [
+            {
+              court: {
+                $regex: req.query.searchText,
+                $options: 'ig',
+              },
+              courtDiv: {
+                $regex: req.query.searchText,
+                $options: 'ig',
+              },
+              'caseParticipants.party': {
+                $regex: req.query.searchText,
+                $options: 'ig',
+              },
+              'caseParticipants.partyDiv': {
+                $regex: req.query.searchText,
+                $options: 'ig',
+              },
+              caseNumber: {
+                $regex: req.query.searchText,
+                $options: 'ig',
+              },
+            },
+          ],
+        })
+          .distinct('_id')
+          .exec();
+
+        const userIDs = User.find({
+          $or: [
+            {
+              firstName: { $regex: req.query.searchText, $options: 'ig' },
+            },
+            {
+              lastName: { $regex: req.query.searchText, $options: 'ig' },
+            },
+          ],
+        })
+          .distinct('_id')
+          .exec();
+
+        const resultObj = await BluePromise.all([caseIDs, userIDs]);
+
+        condition.$or = [
+          {
+            caseId: { $in: resultObj[0] },
+          },
+          {
+            attorney: { $in: resultObj[1] },
+          },
+        ];
+      }
+      if (req.query.caseType) {
+        caseIds = await CaseDetails.find({
+          typeOfCase: req.query.caseType,
+        })
+          .distinct('_id')
+          .exec();
+
+        condition.caseId = { $in: caseIds };
+      }
     }
+
+    lme.i(condition);
 
     const hearingData = await CaseHearing.find(condition)
       .populate([
@@ -59,6 +133,8 @@ export const fetchHearings: RequestHandler = async (req, res, next) => {
       .exec();
 
     const countOfRecords = await CaseHearing.countDocuments(condition).exec();
+
+    lme.i(hearingData.length);
 
     return res.send({
       success: true,
